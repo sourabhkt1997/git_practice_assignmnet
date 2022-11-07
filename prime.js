317500855cbd2139080d75f7abefc60ea628a3f8
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2022-11-07 22:00:12 +0530

    check prime_number problem

---
+++ prime.js
@@ -0,0 +1,17 @@
+
+function check_prime(num){
+  let count=0;
+  for(let i=0;i<=num;i++){
+    if(num%i==0){
+      count++
+    }
+  }
+  return count;
+};
+
+let ans=check_prime(12);
+if(     ){
+  console.log("is prime")
+}else{
+  console.log("not prime")
+}
